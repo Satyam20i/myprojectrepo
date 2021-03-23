fc6cf8b2604c909fb3e4761dfdee071c7f9c2448
Satyam20i <satyamvishwa208@gmail.com>
2021-03-23 09:29:51 +0530

    added a prod function

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -5,13 +5,17 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
-		add(10,20)
+		add(10,20);
 	}
 	public static void add(int num1,int num2){
-		System.out.println(num1+num2)
+		System.out.println(num1+num2);
 	}
 	public static void diff(int num1,int num2){
 		System.out.println(num1-num2);
+	}
+	
+	public static void prod(int num1,int num2){
+		System.out.println(num1*num2);
 	}
 
 }
