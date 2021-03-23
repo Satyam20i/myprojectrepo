9323d0ed1c91da9e60adb6cb322fcd8bd62607e1
Satyam Vishwakarma <47469033+Satyam20i@users.noreply.github.com>
2021-03-23 09:20:15 +0530

    Update Calculator.java

--- SimpleGitDemo/src/main/Calculator.java
+++ SimpleGitDemo/src/main/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
-		add(10,20)
+		add(10,20);
 	}
 	public static void add(int num1,int num2){
 		System.out.println(num1+num2);
